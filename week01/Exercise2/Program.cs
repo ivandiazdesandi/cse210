b8f3345da6f7927e8614cc299ded7028c52a0f73
Ivan <ivandiazdesandi@gmail.com>
2025-07-13 00:36:22 +1000

    Excersices 1 to 4 completed, 5 still pending

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string letter = ("");
+
+        if (grade >= 90)
+        {
+            letter = ("A");
+        }
+        else if (grade >= 80)
+        {
+            letter = ("B");
+        }
+        else if (grade >= 70)
+        {
+            letter = ("C");
+        }
+        else if (grade >= 60)
+        {
+            letter = ("D");
+        }
+        else if (grade < 60)
+        {
+            letter = ("F");
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number form 0 to 100");
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations you have passed!");
+        }
+        else if (grade <= 69)
+        {
+            Console.WriteLine("This is not the end of the road, prepare yourself better and try again!");
+        }
+
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int number = 1;
+        List<int> listNumbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            if (number != 0)
+            {
+                listNumbers.Add(number);
+            }
+        } while (number != 0);
+
+        int total = 0;
+        int largeNumber = 0;
+
+        foreach (int index in listNumbers)
+        {
+            total = index + total;
+            if (index > largeNumber)
+            {
+                largeNumber = index;
+            }
+        }
+
+        float average = ((float)total / listNumbers.Count);
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is: {largeNumber}");
     }
 }
